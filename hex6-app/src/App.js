af6e772e1a7c750976f573aad21cac36a2d9dc79
Adrianna Musialik <musialik.adrianna@gmail.com>
2020-02-23 13:00:47 +0100

    example navigation

--- hex6-app/src/App.js
+++ hex6-app/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
 import './App.css';
+import SegmentExampleRaised from "./components/TripList/TripList.js"
+
+import { BrowserRouter, NavLink, Switch, Route} from "react-router-dom";
 
 
 function App() {
@@ -8,7 +11,20 @@ function App() {
    
     <div className="App">
       <header>
+        <BrowserRouter>
+          <nav>
+            <ul>
+              <li><NavLink exact to="/"><span role="img">🏠</span> Home</NavLink></li>
+              <li><NavLink to="/components/TripList/TripList">Trip List</NavLink></li>
+            </ul>
+          </nav>
         
+        <div className="container">
+          <Switch>
+            <Route path="/components/TripList" component={SegmentExampleRaised} />
+          </Switch>
+        </div>
+        </BrowserRouter>
       </header>
     </div>
   );
