6e9883832fa51e22483d0122eb37fbcc67aedb67
Andrzej Pawlowski <apawlows@icloud.com>
2020-02-23 12:56:38 +0100

    test

--- hex6-app/src/App.js
+++ hex6-app/src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
-
 import './App.css';
-
+import './components/test/SignIn.js';
 
 function App() {
   return (
    
     <div className="App">
       <header>
-        
+     
       </header>
+
+
     </div>
   );
 }
