3be601f219a267dfa36a0adb82aca1f308e7518f
Andrzej Pawlowski <apawlows@icloud.com>
2020-05-17 09:01:29 +0200

    video changed

--- src/components/Dashboard/VideoBox.js
+++ src/components/Dashboard/VideoBox.js
@@ -24,15 +24,15 @@ class VideoBox extends React.Component {
 
   render() {
     return (
-      <video
-        ref={ref => (this.video = ref)}
-        autoPlay
-        muted
-        loop
-        className={styles.videoBox}>
-        <source src="Videos/3city.mp4" type="video/mp4" />
-      </video>
-      // <iframe className={styles.videoBox} src="https://www.youtube-nocookie.com/embed/nTvKj8kOAWk?&mute=1&frameborder=0&autoplay=1&controls=0&showinfo=0&modestbranding=1&loop=1" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe>
+      // <video
+      //   ref={ref => (this.video = ref)}
+      //   autoPlay
+      //   muted
+      //   loop
+      //   className={styles.videoBox}>
+      //   <source src="Videos/3city.mp4" type="video/mp4" />
+      // </video>
+      <iframe className={styles.videoBox} src="https://www.youtube-nocookie.com/embed/nTvKj8kOAWk?&mute=1&frameborder=0&autoplay=1&controls=0&showinfo=0&modestbranding=1&loop=1" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe>
     );
   }
 }
