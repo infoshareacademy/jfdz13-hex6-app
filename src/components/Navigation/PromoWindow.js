6668021e5740ac0ef9d31f71a90d9e687bc16253
Andrzej Pawlowski <apawlows@icloud.com>
2020-05-17 10:27:58 +0200

    side menu go button without login only

--- src/components/Navigation/PromoWindow.js
+++ src/components/Navigation/PromoWindow.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from './Navigation.module.css';
 import Button from '@material-ui/core/Button';
-
+import UserProvider from "../providers/UserProvider";
 
 
   
@@ -10,7 +10,11 @@ import Button from '@material-ui/core/Button';
     <div className={styles.promoWindow}>
           <img style={{ height: 'auto', width:'50%', marginTop:'40px'}} alt="" src="/Graphics/plan-your-trip.svg"></img>
           <h4 style={{ marginTop: '30px', marginBottom: '10px', fontWeight: '400', alignItems:'center'}}>EXPLORE THE TRI CITY WITH US!</h4>
-          <Button variant="outlined" size="small" color="primary" href="/sign-in" >GO NOW</Button>
+    
+          { <UserProvider>
+          {(user) => { return user ? null : <Button variant="outlined" size="small" color="primary" href="/sign-in" >GO NOW</Button> 
+        }}
+          </UserProvider> }
       </div>
     );
   }
